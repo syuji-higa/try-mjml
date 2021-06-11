5ee779e9dfd56a9b81beb8b9f7b2e5eca81bffd5
Syuji Higa <syuji.higa@gmail.com>
2021-06-11 20:02:44 +0900

    feat: add mustache

--- index.mjs
+++ index.mjs
@@ -1,8 +1,10 @@
 import { join } from 'path'
 import { promises as fs } from 'fs'
 import mjml2html from 'mjml'
+import Mustache from 'mustache'
 
 const { mkdir, readFile, writeFile } = fs
+const { render } = Mustache
 
 const SRC_DIR = 'src'
 const DIST_DIR = 'dist'
@@ -11,8 +13,11 @@ const DIST_DIR = 'dist'
   // MJMLファイルの読み込み
   const mjml = await readFile(join(SRC_DIR, 'index.mjml'), 'utf-8')
 
+  // template を生成
+  const { html: template } = mjml2html(mjml)
+
   // html を生成
-  const { html } = mjml2html(mjml)
+  const html = render(template, { name: 'Higa' })
 
   // html を表示
   console.log(html)
